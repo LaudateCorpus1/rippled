cbac51fcbea0103e0a52439b9b20ba46c7a0828d
Howard Hinnant <howard.hinnant@gmail.com>
2019-08-29 17:35:37 -0400

    [FOLD] More std::to_string

--- src/ripple/app/misc/impl/TxQ.cpp
+++ src/ripple/app/misc/impl/TxQ.cpp
@@ -1480,11 +1480,11 @@ TxQ::doRPC(Application& app) const
     auto& levels = ret[jss::levels] = Json::objectValue;
 
     ret[jss::ledger_current_index] = view->info().seq;
-    ret[jss::expected_ledger_size] = to_string(metrics.txPerLedger);
-    ret[jss::current_ledger_size] = to_string(metrics.txInLedger);
-    ret[jss::current_queue_size] = to_string(metrics.txCount);
+    ret[jss::expected_ledger_size] = std::to_string(metrics.txPerLedger);
+    ret[jss::current_ledger_size] = std::to_string(metrics.txInLedger);
+    ret[jss::current_queue_size] = std::to_string(metrics.txCount);
     if (metrics.txQMaxSize)
-        ret[jss::max_queue_size] = to_string(*metrics.txQMaxSize);
+        ret[jss::max_queue_size] = std::to_string(*metrics.txQMaxSize);
 
     levels[jss::reference_level] = to_string(metrics.referenceFeeLevel);
     levels[jss::minimum_level] = to_string(metrics.minProcessingFeeLevel);
